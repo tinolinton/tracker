2b22a251597b8a6d2d3d17a6e2f95ae6684564f8
Linton <dev@chimaliro.com>
2025-07-19 01:37:42 +0200

    hompage

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,33 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import Navbar from "~/Components/Navbar";
+import {resumes} from "../../constants";
+import ResumeCard from "~/Components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resume Tracker" },
+    { name: "description", content: "Welcome to Resume Tracker" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar />
+
+    <section className="main-section py-16">
+      <div className="page-heading">
+        <h1 className="">Track Your Applications & Resume Ratings</h1>
+        <h2>Review your submissions and check AI powered feedback</h2>
+      </div>
+
+
+      {resumes.length > 0 && (
+          <div className="resumes-section">
+            {resumes.map((resume) => (
+                <ResumeCard key={resume.id} resume={resume} />
+            ))}
+          </div>
+      )}
+    </section>
+  </main>
 }
